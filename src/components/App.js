7298e1f97a8e21796d1fb4331b7fc0bee705927d
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-25 22:34:45 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(data=>data.json())
+    .then(data=>{
+      setData(data)
+      setLoading(false)
+    })
+  })
+  if(isLoading) return <h1>Loading...</h1>
   return (
     <div>
         {/* Do not remove the main div */}
+      {JSON.stringify(data)}
     </div>
   )
 }

d8eca02280cef233388a852a78e4d5aaac18ad64
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-30 11:18:01 +0530

    test

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,23 @@ import './../styles/App.css';
 const App = () => {
   const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(true)
-  useEffect(()=>{
+  useEffect(() => {
     fetch('https://dummyjson.com/products')
-    .then(data=>data.json())
-    .then(data=>{
-      setData(data)
-      setLoading(false)
-    })
+      .then(data => data.json())
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+      .catch(err=>setData(err))
   })
-  if(isLoading) return <h1>Loading...</h1>
+  if (isLoading) return <h1>Loading...</h1>
   return (
     <div>
+      <h1>Data Fetched from API</h1>
+      <pre>
         {/* Do not remove the main div */}
-      {JSON.stringify(data)}
+        {JSON.stringify(data)}
+      </pre>
     </div>
   )
 }
